9b1060a5f841fa400d8db88309541a5d3e81e2db
hkuno9000 <hkuno9000@gmail.com>
2016-08-11 17:50:42 +0900

    [REFACTORING] 戦果ゲージ比判定計算から除算を省き、整数演算のみにする. #61
    ゼロ除算対策が不要となった.

--- devtools.js
+++ devtools.js
@@ -1981,10 +1981,10 @@ function guess_win_rank(nowhps, maxhps, beginhps, nowhps_c, maxhps_c, beginhps_c
 	if (e_leader_lost && f_lost_count < e_lost_count) {
 		return 'B';
 	}
-	if (rate > 2.5) { // 確定. rate == 2.5 でC判定を確認済み.
+	if (10 * e_damage_percent > 25 * f_damage_percent) { // 確定. 戦果ゲージ比が2.5より大きいならば、B勝利.
 		return 'B';
 	}
-	if (rate > 0.9) { // 確定. rate == 0.9 でD判定を確認済み.
+	if (10 * e_damage_percent > 9 * f_damage_percent) { // 確定. 戦果ゲージ比が0.9より大きいならば、C敗北.
 		return 'C';
 	}
 	if (f_lost_count < f_count/2) { // 要検証.
