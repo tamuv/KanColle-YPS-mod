c898c702bc49b0ebd16369ee5776d5e49b819434
Hiroshi Kuno <hkuno9000@gmail.com>
2017-02-22 21:14:36 +0900

    refine setver.pl

--- setver.pl
+++ setver.pl
@@ -12,4 +12,5 @@ print "vername :[$ver_name]\n";
 print "version :[$version]\n";
 print "datetime:[$datetime]\n";
 system qq!perl -p -i.bak -e 's/^\\* v\\d.+/* $ver_name: $datetime/ if 2..5;' README.md!;
-system qq!git ci -am 'setver $version' && git tag -f v$version && cp README.md gh-pages/index.md && cd gh-pages && bash autolink.sh && git ci -am 'rel v$version'!;
+system qq!cp README.md gh-pages/index.md && cd gh-pages && bash autolink.sh && git ci -am 'rel v$version'!;
+system qq!git ci -am 'setver $version' && git tag -f v$version!;
