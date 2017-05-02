48fba68bfa7e8a3762daac498e9dfeb37011717b
skywalker999u <skywalker999u@gmail.com>
2017-05-03 05:00:54 +0900

    Fix for 2017 spring E1 first node

--- devtools.js
+++ devtools.js
@@ -1590,6 +1590,7 @@ function on_next_cell(json) {
 		case 1: msg += ':敵影を見ず'; break;
 		case 2: msg += ':能動分岐'; break;
 		case 3: msg += ':穏やかな海です'; break;
+		case 4: msg += ':穏やかな海峡です'; break;
 		default: msg += ':??'; break;
 		}
 		$battle_log.push(msg);
