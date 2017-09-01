7782b33598aed100eb8a53968f4404413cd5ab26
Hiroshi Kuno <hkuno9000@gmail.com>
2017-09-02 00:35:32 +0900

    戦闘結果画面にて、輸送作戦の輸送量を表示する

--- devtools.js
+++ devtools.js
@@ -1961,6 +1961,12 @@ function on_battle_result(json) {
 			}
 		}
 	}
+	if (d.api_landing_hp) {
+		var p = d.api_landing_hp;
+		var s = p.api_now_hp - p.api_sub_value;
+		s = s > 0 ? fraction_percent_name(s, p.api_max_hp) : '達成';
+		req.push('TP: ' + p.api_sub_value + ' => ' + s);
+	}
 	if (drop_ship_name) {
 		req.push('## drop ship', drop_ship_name);
 	}
