4b3e7fe359bc4a9f06ce883b81838a4733455cc4
Hiroshi Kuno <hkuno9000@gmail.com>
2018-03-05 03:51:08 +0900

    Fix: 非戦闘マスの判定ミスを修正した #115

--- devtools.js
+++ devtools.js
@@ -2063,7 +2063,7 @@ function on_next_cell(json) {
 		$battle_log.push(msg);
 		print_next('next loss' + boss_next_name(), msg);
 	}
-	else if (d.api_event_id == 6) {	// 非戦闘マス.
+	else if (d.api_event_id == 1 || d.api_event_id == 6) {	// 非戦闘マス.
 		var msg = area;
 		msg += ':' + event_kind_name(d.api_event_kind);
 		$battle_log.push(msg);
