e1993ca3b999309c711d354532d77164563ceb82
t-f-m <ohmizaiju@s33.xrea.com>
2019-01-02 10:50:16 +0900

    Update: イベント札対応

--- eventnames.js
+++ eventnames.js
@@ -1,11 +1,9 @@
 $event_sally_tag_names = {
-// id: 2018初秋イベントname.
-// http://*/kcs2/img/common/common_event.png?version=4.1.0.0
-1: '警戒部隊',
-2: '海峡派遣艦隊',
-3: '西方作戦部隊',
-4: '欧州特務艦隊',
-5: 'Force H',
-6: 'ライン演習部隊'
+// id: 2019冬イベントname.
+// http://*/kcs2/img/common/common_event.png?version=4.3.0.0
+1: '鼠輸送部隊',
+2: 'ラバウル艦隊',
+3: '連合艦隊',
+4: 'ブイン派遣隊'
 };
 
