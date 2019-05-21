e2505e039e85fbc217ca540f8d59c2ef731f201e
Hiroshi Kuno <hkuno9000@gmail.com>
2019-05-21 10:05:31 +0900

    春イベント2019  前段作戦の札名を更新した.

--- eventnames.js
+++ eventnames.js
@@ -1,9 +1,9 @@
 $event_sally_tag_names = {
-// id: 2019冬イベントname.
-// http://*/kcs2/img/common/common_event.png?version=4.3.0.0
-1: '鼠輸送部隊',
-2: 'ラバウル艦隊',
-3: '連合艦隊',
-4: 'ブイン派遣隊'
+// id: tagname.
+// http://*/kcs2/img/common/common_event.png?version=4.3.7.0
+1: '第百四戦隊', // 春イベント2019 E-1.
+2: '第二艦隊',   // 春イベント2019 E-2.
+3: '北方部隊',   // 春イベント2019 E-3.
+4: '仮札4'
 };
 
