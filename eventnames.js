42fdc60c085ae4044bf6aea518bbd0e88760d8b5
Hiroshi Kuno <hkuno9000@gmail.com>
2019-11-30 16:19:54 +0900

    Update: 秋イベント2019:前段作戦の札名記入.

--- eventnames.js
+++ eventnames.js
@@ -1,9 +1,10 @@
 $event_sally_tag_names = {
 // id: tagname.
-// http://*/kcs2/img/common/common_event.png?version=4.4.4.0
-//1: '欧州防衛艦隊', // 夏イベント2019 E-1.
-//2: '連合艦隊',     // 夏イベント2019 E-2.
-//3: '地中海艦隊',   // 夏イベント2019 E-3.
+// http://*/kcs2/img/common/common_event.png?version=4.5.0.0
+1: '攻略護衛隊', // 秋イベント2019 E-1.
+2: '空襲部隊',   // 秋イベント2019 E-2.
+3: '蘭印部隊',   // 秋イベント2019 E-3.
+4: '馬来部隊',   // 秋イベント2019 E-4.
 9999: null
 };
 
