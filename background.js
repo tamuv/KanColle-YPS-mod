b4fa8e06652ad29689e0b5c82da7ad6240eeb165
Hiroshi Kuno <hkuno9000@gmail.com>
2020-08-19 20:45:32 +0900

    Fix: デイリーパチンコに設置されているリンクへの対策
    commit 74ce8e6ea55e4cb07ab2c8f47985bfdf7919b064
    commit 36a5350b013b1f04aa4e35d5c1373c3e70cc19c0
    にて対策をしたが、manifest.json のみでは不十分だった.

--- background.js
+++ background.js
@@ -1,5 +1,8 @@
 chrome.runtime.onMessage.addListener(function (req) {
-	chrome.tabs.query({url:'http://www.dmm.com/netgame/social/-/gadgets/=/app_id=854854/'}, function (tab) {
+	chrome.tabs.query({url:[
+		'http://www.dmm.com/netgame/social/-/gadgets/=/app_id=854854/',
+		'http://www.dmm.com/netgame/social/-/gadgets/=/app_id=854854'
+		]}, function (tab) {
 		chrome.tabs.sendMessage(tab[0].id, req);
 	});
 });
