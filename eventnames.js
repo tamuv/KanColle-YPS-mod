eccabb5d55998e298a56162679a958cf499a1420
Hiroshi Kuno <hkuno9000@gmail.com>
2020-11-28 04:25:26 +0900

    Update: 晩秋〜冬イベント2020: 後段作戦の札名記入.

--- eventnames.js
+++ eventnames.js
@@ -1,10 +1,11 @@
 $event_sally_tag_names = {
 // id: tagname.
-// http://*/kcs2/img/common/common_event.png?version=5.1.0.0
+// http://*/kcs2/img/common/common_event.png?version=5.1.0.2
 // 晩秋〜冬イベント2020 前段作戦『護衛せよ！船団輸送作戦【欧州編】』
 1: '地中海艦隊',     // E-1.
 2: '護衛R部隊',      // E-2.
 3: 'PQ17船団',       // E-3.
+// 晩秋〜冬イベント2020 後段作戦『護衛せよ！船団輸送作戦【欧州編】』
 4: '南西方面艦隊',   // E-4.
 9999: null
 };
