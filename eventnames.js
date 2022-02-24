ecd4e2ed6ea0846eb494f65ab73de8c27536f171
Hiroshi Kuno <hkuno9000@gmail.com>
2022-02-25 03:30:53 +0900

    Update: 冬イベント2022:前段作戦札名記入(仮)
    E-2以後の札名(tagname)と番号(id)の割当は未検証である.
    札画像の切り出し座標を保持する event.jsonのframes.common_event_* の並び順と、
    id番号の順番が同期していると仮定した. (t-f-mさん情報)

--- eventnames.js
+++ eventnames.js
@@ -1,12 +1,15 @@
 $event_sally_tag_names = {
 // id: tagname.
-// http://*/kcs2/img/common/common_event.png?version=5.3.4.0
-// http://*/kcs2/img/common/common_event.json?version=5.3.4.0
-// 発令！「捷三号作戦警戒」(2022冬イベント)
+// http://*/kcs2/img/common/common_event.png?version=5.3.7.0
+// http://*/kcs2/img/common/common_event.json?version=5.3.7.0
+// 冬イベント2022: 発令！「捷三号作戦警戒」前段作戦
 1: '南西方面艦隊',   // E-1, E-2.
 2: '第三十一戦隊',   // E-2.
 3: '連合艦隊',       // E-3.
 4: '輸送部隊',       // E-3.
+// 冬イベント2022: 発令！「捷三号作戦警戒」後段作戦
+//5: '???', // E-4.
+//6: '???', // E-5.
 9999: null
 };
 
