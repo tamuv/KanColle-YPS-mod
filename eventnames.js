5286df713b4d55a5bf84520737601838d003b5ce
t-f-m <ohmizaiju@s33.xrea.com>
2022-02-25 00:23:32 +0900

    Fix: fix typo

--- eventnames.js
+++ eventnames.js
@@ -4,7 +4,7 @@ $event_sally_tag_names = {
 // http://*/kcs2/img/common/common_event.json?version=5.3.4.0
 // 発令！「捷三号作戦警戒」(2022冬イベント)
 1: '南西方面艦隊',   // E-1, E-2.
-2: '第三十一船体',   // E-2.
+2: '第三十一戦隊',   // E-2.
 3: '連合艦隊',       // E-3.
 4: '輸送部隊',       // E-3.
 9999: null
