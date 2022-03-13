c870da249666a142edfe2e928c711023e0ff58b1
Hiroshi Kuno <hkuno9000@gmail.com>
2022-03-13 16:20:03 +0900

    Update: Fix: 冬イベント2022:作戦札名.

--- eventnames.js
+++ eventnames.js
@@ -1,12 +1,13 @@
 $event_sally_tag_names = {
 // id: tagname.
-// http://*/kcs2/img/common/common_event.png?version=5.3.7.0
-// http://*/kcs2/img/common/common_event.json?version=5.3.7.0
+// http://*/kcs2/img/common/common_event.png?version=5.3.7.2
+// http://*/kcs2/img/common/common_event.json?version=5.3.7.2
+// jsonのcommon_event_番号並びと、札ID番号並びは一致している.
 // 冬イベント2022: 発令！「捷三号作戦警戒」前段作戦
 1: '南西方面艦隊',   // E-1, E-2, E-4.
 2: '第三十一戦隊',   // E-2.
-3: '連合艦隊',       // E-3, E-5.
-4: '輸送部隊',       // E-3.
+3: '輸送部隊',       // E-3.
+4: '連合艦隊',       // E-3, E-5.
 // 冬イベント2022: 発令！「捷三号作戦警戒」後段作戦
 5: '第十六戦隊',     // E-4.
 6: 'シマ船団護衛',   // E-4.
