79d2e330924c61c38e5dddb457c9a25ef777b94c
Hiroshi Kuno <hkuno9000@gmail.com>
2022-12-29 15:35:44 +0900

    Fix: #180 Service Worker 無効対応.

--- background.js
+++ background.js
@@ -1,3 +1,7 @@
+try {
+// "service worker (無効)" の回避策として try/catch で囲む.
+// @see https://github.com/furyutei/twMediaDownloader/issues/89#issuecomment-1261621380
+// @see https://groups.google.com/a/chromium.org/g/chromium-extensions/c/lLb3EJzjw0o
 chrome.runtime.onMessage.addListener(function (req) {
 	chrome.tabs.query({url:[
 		'http://www.dmm.com/netgame/social/-/gadgets/=/app_id=854854/',
@@ -6,3 +10,6 @@ chrome.runtime.onMessage.addListener(function (req) {
 		chrome.tabs.sendMessage(tab[0].id, req);
 	});
 });
+} catch (e) {
+	console.error(e);
+}
