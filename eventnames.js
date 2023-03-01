21c20cd63698af51d6167aad53a65229fedf1fc5
Hiroshi Kuno <hkuno9000@gmail.com>
2023-03-01 21:28:36 +0900

    Feat: typofix イベンド札名

--- eventnames.js
+++ eventnames.js
@@ -6,7 +6,7 @@ $event_sally_tag_names = {
 // 下記コードにて番号並び順に画像切り取り座標を抜き出し、その位置に描かれている札名を列記する.
 // v = {}; for (i in common_event.frames) { v[i] = common_event.frames[i].frame; }; console.table(v)
 // 早春イベント2023: 前段作戦【絶対防衛線！「小笠原兵団」救援】
-1: '横須賀防衛戦隊',
+1: '横須賀防備戦隊',
 2: '第三水雷戦隊',
 3: '支援連合艦隊',
 4: '連合艦隊',
