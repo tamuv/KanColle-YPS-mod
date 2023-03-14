cbf9c65147dd6fbe4117aa4c27c61d5e781621c2
Hiroshi Kuno <hkuno9000@gmail.com>
2023-03-14 22:01:18 +0900

    Feat: typofix イベント札名

--- eventnames.js
+++ eventnames.js
@@ -7,7 +7,7 @@ $event_sally_tag_names = {
 // v = {}; for (i in common_event.frames) { v[i] = common_event.frames[i].frame; }; console.table(v)
 // 早春イベント2023: 前段作戦【絶対防衛線！「小笠原兵団」救援】
 1: '横須賀防備戦隊',
-2: '第三水雷戦隊',
+2: '第二水雷戦隊',
 3: '支援連合艦隊',
 4: '連合艦隊',
 5: '空母機動部隊',
