1540ff9702fe525c17ed1c2e1b47faaf5636d425
Hiroshi Kuno <hkuno9000@gmail.com>
2023-08-27 12:27:25 +0900

    Update: 夏イベント2023:後段作戦札名記入.

--- eventnames.js
+++ eventnames.js
@@ -1,7 +1,7 @@
 $event_sally_tag_names = {
 // id: tagname.
-// http://*/kcs2/img/common/common_event.png?version=5.7.0.0
-// http://*/kcs2/img/common/common_event.json?version=5.7.0.0
+// http://*/kcs2/img/common/common_event.png?version=5.7.0.5
+// http://*/kcs2/img/common/common_event.json?version=5.7.0.5
 // jsonのcommon_event_番号並びと、札ID番号並びは一致している.
 // 下記コードにて番号並び順に画像切り取り座標を抜き出し、その位置に描かれている札名を列記する.
 // v = {}; for (i in common_event.frames) { v[i] = common_event.frames[i].frame; }; console.table(v)
@@ -12,6 +12,12 @@ $event_sally_tag_names = {
 4: '第一水雷戦隊',
 5: '第五艦隊主力',
 6: '第一艦隊',
+// 夏イベント2023: 後段作戦【船団護衛！輸送航路防衛戦】
+7: '欧州遠征先遣隊',
+8: 'イギリス連絡部隊',
+9: '英海軍主力部隊',
+10: '英D-Day上陸部隊',
+11: '米D-Day上陸部隊',
 9999: null
 };
 
